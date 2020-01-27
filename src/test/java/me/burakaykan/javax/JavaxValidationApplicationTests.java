330c110409755bd72c1ee85e67e0a004adaebee0
Burak Aykan ÜRER <burakaykan2@gmail.com>
2020-01-27 23:02:49 +0300

    :wrench: configuration files

---
+++ src/test/java/me/burakaykan/javax/JavaxValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.burakaykan.javax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaxValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
