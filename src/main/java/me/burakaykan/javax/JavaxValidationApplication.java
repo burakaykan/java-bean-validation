a15f6c19549f094e77fbdca8652df981ede64780
Burak Aykan ÜRER <burakaykan2@gmail.com>
2020-01-27 23:05:16 +0300

    Main class & .yml configuration

---
+++ src/main/java/me/burakaykan/javax/JavaxValidationApplication.java
@@ -0,0 +1,13 @@
+package me.burakaykan.javax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaxValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaxValidationApplication.class, args);
+	}
+
+}
